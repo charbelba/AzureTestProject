9f5c1b8707394f67ef2538b502a0417d49688adf
charbel bou abdo <120515242+Glompis@users.noreply.github.com>
2024-03-08 16:57:13 +0200

    Initial

---
+++ src/test/java/com/example/AzureTestProject/AzureTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
